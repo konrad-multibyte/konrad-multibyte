2fd662215a7a678e079a05833da6a0ab7e668190
Konrad Gladysz <16876426+konrad-multibyte@users.noreply.github.com>
2023-12-16 22:56:49 +0000

    js for dark mode

---
+++ main.js
@@ -0,0 +1,53 @@
+const getPerferedColorScheme = () => window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches ? 'dark' : 'light';
+
+const colorMap = [
+    {
+        id: "darkMode",
+        light: ["primary-5"],
+        dark: ["primary-1"]
+    },
+    {
+        id: "right",
+        light: ["primary-5"],
+        dark: ["primary-1"]
+    }
+]
+
+function setColorScheme(darkModeControl) {
+    if (darkModeControl) {
+        for (let colorItem of colorMap) {
+            for (let className of colorItem.dark) {
+                document.getElementById(colorItem.id).classList.add(className);
+            }
+            for (let className of colorItem.light) {
+                document.getElementById(colorItem.id).classList.remove(className);
+            }
+        }
+    } else {
+        for (let colorItem of colorMap) {
+            for (let className of colorItem.light) {
+                document.getElementById(colorItem.id).classList.add(className);
+            }
+            for (let className of colorItem.dark) {
+                document.getElementById(colorItem.id).classList.remove(className);
+            }
+        }
+    }
+}
+
+function switchColorScheme() {
+    localStorage.setItem("darkModeControl", document.getElementById("darkModeControl").checked);
+    setColorScheme(document.getElementById("darkModeControl").checked);
+}
+
+function setInitialColorScheme() {
+    if (localStorage.getItem("darkModeControl") == null) {
+        // localStorage.setItem("darkModeControl", getPerferedColorScheme() == "dark" ? true : false);
+    }
+    document.getElementById("darkModeControl").checked = localStorage.getItem("darkModeControl");
+    setColorScheme(localStorage.getItem("darkModeControl"));
+}
+
+document.getElementById("darkModeControl").addEventListener("change", switchColorScheme);
+
+setInitialColorScheme();
